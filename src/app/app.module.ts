fd6d1da04adb5f43ca9bf73bc165eb4ff26b690f
Yongkyun Lee <ylee2@caltech.edu>
2020-12-22 01:57:39 +0900

    Setup firestore and blog home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -25,6 +29,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
     AppRoutingModule,
     MatSidenavModule,
     MatButtonModule,
---
+++ src/app/blog/models/blog-snippet.model.ts
@@ -0,0 +1,8 @@
+export interface BlogSnippet {
+    titleEng: string;
+    titleKor: string;
+    date: string;
+    keywordsEng: string[];
+    keywordsKor: string[];
+    category: string;
+}
