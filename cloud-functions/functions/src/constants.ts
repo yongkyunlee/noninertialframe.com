ea5de6209d2c57defca002de9ac611bf23b3066f
Yongkyun Lee <ylee2@caltech.edu>
2020-12-31 05:29:14 +0900

    Add user activity for comments

---
+++ cloud-functions/functions/src/constants.ts
@@ -0,0 +1,3 @@
+export const BLOG_COLLECTION = 'blog-snippets';
+
+export const USER_ACTIVITY_COLLECTION = 'user-activity';
--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,14 +1,18 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { BLOG_COLLECTION, USER_ACTIVITY_COLLECTION } from './constants';
+
 admin.initializeApp();
 
 const auth = admin.auth();
+
 
 interface ClaimsDocumentData extends admin.firestore.DocumentData {
     _lastCommitted?: admin.firestore.Timestamp
 }
 
-export const mirrorCustomClaims = functions.firestore.document('user-claims/{uid}')
+export const mirrorCustomClaims = functions.firestore
+    .document('user-claims/{uid}')
     .onWrite(async (change, context) => {
         const beforeData: ClaimsDocumentData = change.before.data() || {};
         const afterData: ClaimsDocumentData = change.after.data() || {};
@@ -34,4 +38,78 @@ export const mirrorCustomClaims = functions.firestore.document('user-claims/{uid
             _lastCommitted: admin.firestore.FieldValue.serverTimestamp(),
             ...newClaims,
         });
+    });
+
+export const recordCreateComment = functions.firestore
+    .document(`${BLOG_COLLECTION}/{postId}/comments/{commentId}`)
+    .onCreate((snap, context) => {
+        const newComment = snap.data();
+        const newActivity = {
+            action: 'create',
+            timestamp: newComment.timestamp,
+            postId: context.params.postId,
+            commentId: context.params.commentId,
+            content: newComment.content,
+            createdAt: newComment.timestamp,
+            nickname: newComment.nickname,
+        };
+
+        admin.firestore().collection(USER_ACTIVITY_COLLECTION).doc(newComment.userId)
+                .collection('comments').add(newActivity)
+            .then(() => {
+                console.log(newActivity);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    });
+
+export const recordUpdateComment = functions.firestore
+    .document(`${BLOG_COLLECTION}/{postId}/comments/{commentId}`)
+    .onUpdate((change, context) => {
+        const newComment = change.after.data();
+        const previousComment = change.before.data();
+        const newActivity = {
+            action: 'update',
+            timestamp: admin.firestore.FieldValue.serverTimestamp(),
+            postId: context.params.postId,
+            commentId: context.params.commentId,
+            newContent: newComment.content,
+            previousContent: previousComment.content,
+            createdAt: previousComment.timestamp,
+            nickname: previousComment.nickname,
+        };
+
+        admin.firestore().collection(USER_ACTIVITY_COLLECTION).doc(previousComment.userId)
+                .collection('comments').add(newActivity)
+            .then(() => {
+                console.log(newActivity);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    });
+
+export const recordDeleteComment = functions.firestore
+    .document(`${BLOG_COLLECTION}/{postId}/comments/{commentId}`)
+    .onDelete((snap, context) => {
+        const deletedComment = snap.data();
+        const newActivity = {
+            action: 'delete',
+            timestamp: admin.firestore.FieldValue.serverTimestamp(),
+            postId: context.params.postId,
+            commentId: context.params.commentId,
+            content: deletedComment.content,
+            createdAt: deletedComment.timestamp,
+            nickname: deletedComment.nickname,
+        };
+
+        admin.firestore().collection(USER_ACTIVITY_COLLECTION).doc(deletedComment.userId)
+                .collection('comments').add(newActivity)
+            .then(() => {
+                console.log(newActivity);
+            })
+            .catch(err => {
+                console.error(err);
+            });
     });
