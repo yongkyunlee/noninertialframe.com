a86e11a316aa444432ec85e159192a789e4e78a8
Yongkyun Lee <yongkyun.daniel.lee@gmail.com>
2021-12-01 02:24:13 -0500

    Added paper to project link option

--- client/src/app/admin/projects/new-project/new-project.component.ts
+++ client/src/app/admin/projects/new-project/new-project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProjectSnippet } from 'src/app/projects/projects.model';
 import { EditProjectComponent } from '../edit-project/edit-project.component';
 
